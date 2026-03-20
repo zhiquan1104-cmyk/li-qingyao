d06fb2061a25bde71eda02f2bfc986dd5136e159
zhiquan1104-cmyk <zhiquan1104@gmail.com>
2026-03-20 07:17:52 +0000

    Deploying to gh-pages from @ zhiquan1104-cmyk/li-qingyao@3b2b9b8510d9b6804a45375790564748dbc2e3c6 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://liqingyao666-cmyk.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://liqingyao6666-cmyk.github.io/al-folio/#contact">contact us</a>.'
               }
             ]
           }
